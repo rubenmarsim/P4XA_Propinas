1346c165383645d09f8adf50451dc07c223d756c
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-12 18:27:21 +0100

    word

--- Propinas_XA/Propinas_XA/MainActivity.cs
+++ Propinas_XA/Propinas_XA/MainActivity.cs
@@ -3,17 +3,98 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
 
 namespace Propinas_XA
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TipInfo info = new TipInfo()
+        {
+            TipPercent = 15,
+        };
+
+        TextView TipPercent;
+        TextView Total;
+        TextView TipValue;
+
+        public MainActivity()
+        {
+            info.TipValueChanged += (sender, e) =>
+            {
+                TipValue.Text = info.TipValue.ToString();
+                Total.Text = (info.TipValue + info.Total).ToString();
+            };
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            TipValue = FindViewById<TextView>(Resource.Id.txtVwTipValue);
+            Total = FindViewById<TextView>(Resource.Id.txtVwTotal);
+            TipPercent = FindViewById<TextView>(Resource.Id.txtVwTipPercent);
+
+            TipPercent.AfterTextChanged += (sender, e) =>
+            {
+                info.TipPercent = Parse(TipPercent);
+            };
+
+            FindViewById<SeekBar>(Resource.Id.slider).SetOnSeekBarChangeListener(new SeekBarChangeListener(this));
+
+            var subtotal = FindViewById<TextView>(Resource.Id.txtVwSubtotal);
+            subtotal.AfterTextChanged+=(sender, e) =>
+            {
+                info.Subtotal = Parse(Total);
+            };
+            var total = FindViewById<TextView>(Resource.Id.txtVwTotalPostTax);
+            total.AfterTextChanged += (sender, e) =>
+              {
+                  info.Total = Parse(total);
+              };
+        }
+
+        static decimal Parse(TextView field)
+        {
+            if (field.Text == "")
+            {
+                return 0m;
+            }
+            try
+            {
+                return Convert.ToDecimal(field.Text);
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+
+                field.Text = string.Empty;
+                return 0m;
+            }
+        }
+    }
+
+    class SeekBarChangeListener:Java.Lang.Object, SeekBar.IOnSeekBarChangeListener
+    {
+        MainActivity context;
+
+        public SeekBarChangeListener(MainActivity context)
+        {
+            this.context = context;
+        }
+        public void OnProgressChanged(SeekBar seekBar, int progress, bool fromUser)
+        {
+            context.TipPercent.text = progress.ToString();
+        }
+        public void OnStartTrackingTouch(SeekBar seekBar)
+        {
+
+        }
+        public void OnStopTrackingTouch(SeekBar seekBar)
+        {
+
         }
     }
 }
---
+++ Propinas_XA/Propinas_XA/TipInfo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Propinas_XA
+{
+    class TipInfo
+    {
+        decimal _Total;
+        public decimal Total
+        {
+            get { return _Total; }
+            set
+            {
+                if (_Total != value)
+                {
+                    _Total = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        decimal _Subtotal;
+        public decimal Subtotal
+        {
+            get { return _Subtotal; }
+            set
+            {
+                if (_Subtotal != value)
+                {
+                    _Subtotal = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        decimal _TipPercent;
+        public decimal TipPercent
+        {
+            get { return _TipPercent; }
+            set
+            {
+                if (_TipPercent != value)
+                {
+                    _TipPercent = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        private void OnTipValueChanged()
+        {
+            var h = TipValueChanged;
+            if (h != null)
+            {
+                h(this, EventArgs.Empty);
+            }
+        }
+
+        public decimal Tax
+        {
+            get { return Total - Subtotal; }
+        }
+
+        public decimal TipValue
+        {
+            get
+            {
+                if(Total == 0m || Subtotal == 0m || TipPercent == 0m)
+                {
+                    return 0m;
+                }
+
+                var percent = TipPercent;
+                percent /= 100;
+                decimal value = (Subtotal * (1 + percent)) + (Total - Subtotal);
+                decimal fract = value - Math.Truncate(value);
+                int f = (int)(fract * 100);
+                while ((f % 25) != 0)
+                {
+                    f++;
+
+                }
+                fract = f;
+                fract /= 100;
+                value = Math.Truncate(value) + fract;
+                return value - Total;
+            }
+        }
+
+        public event EventHandler TipValueChanged;
+    }
+}
